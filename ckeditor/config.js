513265b0e1b21ac6741aa0296eb689e7fc0e04e3
Michael Brinkmeier <mbrinkmeier@uos.de>
2018-12-06 15:44:29 +0100

    * ckeditor config changed

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -17,106 +17,105 @@
  * limitations under the License.
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-	// %REMOVE_START%
-	config.plugins =
-		'about,' +
-		'a11yhelp,' +
-		'basicstyles,' +
-		'bidi,' +
-		'blockquote,' +
-		'clipboard,' +
-		'colorbutton,' +
-		'colordialog,' +
-		'copyformatting,' +
-		'contextmenu,' +
-		'dialogadvtab,' +
-		'div,' +
-		'elementspath,' +
-		'enterkey,' +
-		'entities,' +
-		'filebrowser,' +
-		'find,' +
-		'flash,' +
-		'floatingspace,' +
-		'font,' +
-		'format,' +
-		'forms,' +
-		'horizontalrule,' +
-		'htmlwriter,' +
-		'image,' +
-		'iframe,' +
-		'indentlist,' +
-		'indentblock,' +
-		'justify,' +
-		'language,' +
-		'link,' +
-		'list,' +
-		'liststyle,' +
-		'magicline,' +
-		'maximize,' +
-		'newpage,' +
-		'pagebreak,' +
-		'pastefromword,' +
-		'pastetext,' +
-		'preview,' +
-		'print,' +
-		'removeformat,' +
-		'resize,' +
-		'save,' +
-		'selectall,' +
-		'showblocks,' +
-		'showborders,' +
-		'smiley,' +
-		'sourcearea,' +
-		'specialchar,' +
-		'stylescombo,' +
-		'tab,' +
-		'table,' +
-		'tableselection,' +
-		'tabletools,' +
-		'templates,' +
-		'toolbar,' +
-		'undo,' +
-		'uploadimage,' +
-		'wysiwygarea';
-	// %REMOVE_END%
+CKEDITOR.editorConfig = function (config) {
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+    // %REMOVE_START%
+    config.plugins =
+            'about,' +
+            'a11yhelp,' +
+            'basicstyles,' +
+            'bidi,' +
+            'blockquote,' +
+            'clipboard,' +
+            'colorbutton,' +
+            'colordialog,' +
+            'copyformatting,' +
+            'contextmenu,' +
+            'dialogadvtab,' +
+            'div,' +
+            'elementspath,' +
+            'enterkey,' +
+            'entities,' +
+            'filebrowser,' +
+            'find,' +
+            'flash,' +
+            'floatingspace,' +
+            'font,' +
+            'format,' +
+            'forms,' +
+            'horizontalrule,' +
+            'htmlwriter,' +
+            'image,' +
+            'iframe,' +
+            'indentlist,' +
+            'indentblock,' +
+            'justify,' +
+            'language,' +
+            'link,' +
+            'list,' +
+            'liststyle,' +
+            'magicline,' +
+            'maximize,' +
+            'newpage,' +
+            'pagebreak,' +
+            'pastefromword,' +
+            'pastetext,' +
+            'preview,' +
+            'print,' +
+            'removeformat,' +
+            'resize,' +
+            'save,' +
+            'selectall,' +
+            'showblocks,' +
+            'showborders,' +
+            'smiley,' +
+            'sourcearea,' +
+            'specialchar,' +
+            'stylescombo,' +
+            'tab,' +
+            'table,' +
+            'tableselection,' +
+            'tabletools,' +
+            'templates,' +
+            'toolbar,' +
+            'undo,' +
+            'uploadimage,' +
+            'wysiwygarea';
+    // %REMOVE_END%
 
 
-		config.allowedContent = true;
-	  config.pasteFilter = null;
-	  config.extraPlugins ='abbozza';
-	  config.extraAllowedContent = 'abbozza-hint page pagescript taskscript';
-	  config.resize_enabled = false;
-	  config.removePlugins = "resize";
-		config.toolbarCanCollapse= true;
-		config.toolbarStartupExpanded = true;
-		config.width = "100%";
-		config.height = "100%";
+    config.allowedContent = true;
+    config.pasteFilter = null;
+    config.extraPlugins = 'abbozza';
+    config.extraAllowedContent = 'abbozza-hint page pagescript taskscript';
+    config.resize_enabled = true;
+    config.toolbarCanCollapse = true;
+    config.toolbarStartupExpanded = true;
+    config.width = "100%";
+    config.height = "400px";
 
-	  config.toolbarGroups = [
-			{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-			{ name: 'undo', groups: [ 'undo' ] },
-			{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-			// { name: 'forms', groups: [ 'forms' ] },
-			{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-			'/',
-			{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'paragraph' ] },
-			{ name: 'links', groups: [ 'links' ] },
-			{ name: 'insert', groups: [ 'insert' ] },
-			{ name: 'abbozza', groups: [ 'abbozza' ] },
-			'/',
-			{ name: 'styles', groups: [ 'styles' ] },
-			{ name: 'colors', groups: [ 'colors' ] },
-			{ name: 'tools', groups: [ 'tools' ] },
-			{ name: 'others', groups: [ 'others' ] },
-			{ name: 'about', groups: [ 'about' ] }
-		];
+    config.toolbarGroups = [
+        {name: 'document', groups: ['mode', 'document', 'doctools']},
+        {name: 'undo', groups: ['undo']},
+        {name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing']},
+        // { name: 'forms', groups: [ 'forms' ] },
+        {name: 'basicstyles', groups: ['basicstyles', 'cleanup']},
+        '/',
+        {name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'paragraph']},
+        {name: 'links', groups: ['links']},
+        {name: 'insert', groups: ['insert']},
+        {name: 'abbozza', groups: ['abbozza']},
+        '/',
+        {name: 'styles', groups: ['styles']},
+        {name: 'colors', groups: ['colors']},
+        {name: 'tools', groups: ['tools']},
+        {name: 'others', groups: ['others']},
+        {name: 'about', groups: ['about']}
+    ];
 
-		config.removeButtons = 'Save,Preview,Print,Templates,newPage,Find,Replace,bidi,Flash,pagebreak';
+    config.removeButtons = 'Save,Preview,Print,Templates,newPage,Find,Replace,bidi,Flash,pagebreak';
 
 
 };
